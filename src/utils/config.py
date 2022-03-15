33eddeffe6473a78757568f1fe8c4fc3d9d146d2
Ritesh Soun <sounritesh@gmail.com>
2022-03-15 21:22:41 +0530

    TPU

--- src/utils/config.py
+++ src/utils/config.py
@@ -1,3 +1,5 @@
 import torch
+import torch_xla.core.xla_model as xm
 
-DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
+# DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
+DEVICE = xm.xla_device()
