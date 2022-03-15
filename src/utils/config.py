57f0bf22d05c919637695affac5530cacc79c7fc
Ritesh Soun <sounritesh@gmail.com>
2022-03-15 21:26:42 +0530

    bug fix

--- src/utils/config.py
+++ src/utils/config.py
@@ -1,5 +1,5 @@
 import torch
-import torch_xla.core.xla_model as xm
+# import torch_xla.core.xla_model as xm
 
-# DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
-DEVICE = xm.xla_device()
+DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
+# DEVICE = xm.xla_device()
