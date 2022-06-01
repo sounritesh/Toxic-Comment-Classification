8df56f46e5d2fc4c63b485273eab047ec4c7f669
sounritesh <sounritesh@gmail.com>
2022-06-01 17:31:58 +0530

    prep function call corrected

--- src/data/dataset.py
+++ src/data/dataset.py
@@ -62,7 +62,7 @@ class ToxicityDatasetBERT(Dataset):
         text = self.texts[index]
 
         if self.preprocess:
-            text = self.preprocess(text).strip()
+            text = self.preprocess_text(text).strip()
 
         inputs = self.tokenizer.encode_plus(
             text,
